29757b5202db4a6b26ab295ce3daa4354e9e5031
conconartist <conconart.info@gmail.com>
2021-03-09 09:50:15 -0700

    Add UrlCard component

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './UrlContainer.css';
+import UrlCard from '../UrlCard/UrlCard';
 
-const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+const UrlContainer = ({urls})=> {
+  console.log(urls)
+  const urlEls = urls.map(url => {
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
-      </div>
+      <UrlCard 
+        id={url.id}
+        title={url.title}
+        short_url={url.short_url}
+        long_url={url.long_url}
+      />
     )
   });
 
