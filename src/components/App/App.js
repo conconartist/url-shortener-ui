1f5fa05a488390dd9cd5c7ed1d10cf06f691de94
conconartist <conconart.info@gmail.com>
2021-03-09 11:12:32 -0700

    Refactor componentdidMount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getUrls } from '../../apiCalls';
+import { getUrls, postUrl } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
@@ -8,31 +8,35 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isFetching: false,
+      error: false, 
       urls: []
     }
   }
 
   componentDidMount() {
+    this.setState({isFetching: true})
     return getUrls()
     .then(data => {
-      this.setState({urls: data.urls})
+      this.setState({urls: data.urls, isFetching: false})
     }).catch(error => {
       console.log(error)
+      this.setState({isFetching: false, error: true})
     })
   }
   
   addUrl = (newUrl) => {
     this.setState({urls: [...this.state.urls, newUrl]})
+    postUrl(newUrl.urlToShorten, newUrl.title)
   }
-  
+
   render() {
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addUrl={this.addUrl}/>
         </header>
-
         <UrlContainer urls={this.state.urls}/>
       </main>
     );
