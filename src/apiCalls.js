119da889a1af0e532be26aa03ff895adc3deb8e2
conconartist <conconart.info@gmail.com>
2021-03-09 10:15:40 -0700

    Add post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (long_url, title) => {
+  const data = {long_url: long_url, title: title}
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log('Success:', data)
+  })
+  .catch((error) => {
+    console.log('Error:', data)
+  })
+}
