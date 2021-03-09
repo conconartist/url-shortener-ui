54d2002b35edc700f6140867842ee6672e3a35c8
conconartist <conconart.info@gmail.com>
2021-03-09 11:12:14 -0700

    Refactor apiCall

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,5 +9,11 @@ export const postUrl = (long_url, title) => {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify(data)
+  }).then((data) => {
+    console.log('Success:', data)
+  
+  })
+  .catch((error) => {
+    console.log('Error:', data)
   })
 }
